b59aa5d0cb6f21a20937bab467d77ff4773c8f42
Xenousss <ttng001@gmail.com>
2020-08-17 16:52:41 +0800

    Updated Main Menu Scene By Ng Ting Wei
    Updated the Main Menu UI with level popup.
    There is also an update on the scene manager whcch will load into level 1, 2, 3 using the start button, level select button and quit button.

--- Tower Defence Sanctuary/Assets/Scripts/mainmenuScript.cs
+++ Tower Defence Sanctuary/Assets/Scripts/mainmenuScript.cs
@@ -6,10 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class mainmenuScript : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public GameObject instructionmenu;
+    public GameObject levelMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instructionmenu != null)
+            instructionmenu.SetActive(false);
+        if (levelMenu != null)
+            levelMenu.SetActive(false);
+
+        levelMenu.SetActive(false);
+        instructionmenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,24 +30,46 @@ public class mainmenuScript : MonoBehaviour
         
     }
 
-    public void startGame()
+    IEnumerator Loadlevel(string levelname)
     {
-
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelname);
     }
 
-    public void loadLevel()
+    public void startGame()
     {
+        StartCoroutine(Loadlevel("Level_1"));
+    }
 
+    public void mainmenu()
+    {
+        StartCoroutine(Loadlevel("MainMenu"));
+    }
+
+    public void loadLevel(string levelName)
+    {
+        StartCoroutine(Loadlevel(levelName));
+    }
+
+    public void onLevelmenu()
+    {
+        levelMenu.SetActive(true);
+    }
+
+    public void offLevelmenu()
+    {
+        levelMenu.SetActive(false);
     }
 
     public void onInstruction()
     {
-
+        instructionmenu.SetActive(true);
     }
 
     public void offInstruction()
     {
-
+        instructionmenu.SetActive(false);
     }
 
     public void quitGame()
