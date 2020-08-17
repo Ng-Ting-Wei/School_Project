a1da345d9c90ab939d5f08d1d5adab776daaa4aa
Xenousss <ttng001@gmail.com>
2020-08-17 15:41:21 +0800

    Updated Main Menu by Ng Ting WEi
    I updated the Main menu and some audio.
    By Ng Ting Wei

---
+++ Tower Defence Sanctuary/Assets/Scripts/mainmenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainmenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+
+    }
+
+    public void loadLevel()
+    {
+
+    }
+
+    public void onInstruction()
+    {
+
+    }
+
+    public void offInstruction()
+    {
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
