c1c5dbbf191fb1952ee5438ffe4d93cb99043a0f
Xenousss <ttng001@gmail.com>
2020-08-24 16:11:36 +0800

    Updated the HealthBar UI by Ng Ting Wei
    Finished updated the HealthBar UI of the player base.

--- Tower Defence Sanctuary/Assets/Scripts/PlayerBase.cs
+++ Tower Defence Sanctuary/Assets/Scripts/PlayerBase.cs
@@ -8,20 +8,23 @@ public class PlayerBase : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    public Playerhealthbar healthbar;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthbar.setmaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             takeDamage(20);
         }
-        if (Input.GetKey(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             healDamage(20);
         }
@@ -30,10 +33,22 @@ public class PlayerBase : MonoBehaviour
     public void takeDamage(int damage)
     {
         currentHealth -= damage;
+        healthbar.setHealth(currentHealth);
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
     }
 
     public void healDamage(int heal)
     {
         currentHealth += heal;
+        healthbar.setHealth(currentHealth);
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
     }
 }
--- Tower Defence Sanctuary/Assets/Scripts/Playerhealthbar.cs
+++ Tower Defence Sanctuary/Assets/Scripts/Playerhealthbar.cs
@@ -7,14 +7,20 @@ public class Playerhealthbar : MonoBehaviour
 {
     public Slider slider;
 
+    public Gradient gradient;
+    public Image fill;
+
     public void setmaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void setHealth(int health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
